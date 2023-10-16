0c408fab8e69765632a6dddbaeb39dd105916d87
Julien Denozi <j-denozi@innopsys.fr>
2023-10-16 14:38:19 +0200

    Init repository for game refonte
    Signed-off-by: Julien Denozi <j-denozi@innopsys.fr>

---
+++ README.md
@@ -0,0 +1,14 @@
+# Realm: End of Era (Currently in development)
+
+![Realm: End of Era](assets/images/readme-game-logo.gif)
+
+Realm: End of Era is an immersive trading card game set in a fantastical world where players engage in strategic battles, collect powerful cards, and unravel the mysteries of the ancient realms. This game is built using React for the front-end and Node.js for the back-end.
+
+## Features
+
+- **Card Battles**: Experience thrilling one-on-one battles with your customized deck of powerful cards.
+- **Card Collection**: Discover and collect a wide variety of unique cards with different abilities and attributes.
+- **Strategic Gameplay**: Develop your own tactics, build synergistic decks, and outsmart your opponents.
+- **Quests and Adventures**: Embark on epic quests and challenging adventures to earn rewards and unlock new cards.
+- **Player Rankings**: Compete against other players and climb the global leaderboards.
+- **Community Features**: Engage with a vibrant community of players, trade cards, and participate in tournaments.
---
+++ cmake/AutoQtQml.cmake
@@ -0,0 +1,73 @@
+function(link_qt_common_to_target TARGET)
+
+    find_package(Qt5 COMPONENTS ${QTCOMMON_QT_COMPONENTS} REQUIRED)
+    foreach(QT_COMPONENT ${QTCOMMON_QT_COMPONENTS})
+        message("-- [Link] Qt5::${QT_COMPONENT}")
+        target_link_libraries(${TARGET} PRIVATE "Qt5::${QT_COMPONENT}")
+        target_include_directories(${TARGET} PRIVATE ${Qt5${QT_COMPONENT}_INCLUDE_DIRS})
+    endforeach()
+
+    # Create modules qmldir files
+    function(create_modules_qmldir_files ROOT_DIR)
+        file(GLOB MODULE_FILES "${ROOT_DIR}/*")
+        list(FILTER MODULE_FILES EXCLUDE REGEX "(qmldir$)|(internal$)")
+
+        foreach(MODULE_FILE ${MODULE_FILES})
+            if(IS_DIRECTORY "${MODULE_FILE}")
+                list(FILTER MODULE_FILES EXCLUDE REGEX "^${MODULE_FILE}$")
+                create_modules_qmldir_files("${MODULE_FILE}")
+            endif()
+        endforeach()
+
+        list(FILTER MODULE_FILES INCLUDE REGEX "\\.qml$")
+
+        if (NOT MODULE_FILES)
+            return()
+        endif()
+
+        file(GLOB MODULES_INTERNAL_FILES "${ROOT_DIR}/internal/*")
+
+        get_filename_component(MODULE_NAME "${ROOT_DIR}" NAME)
+        set(QMLDIR_FILE_CONTENT "module ${MODULE_NAME}\n")
+        foreach(MODULE_FILE ${MODULE_FILES})
+            file(READ "${MODULE_FILE}" MODULE_FILE_CONTENT LIMIT 16)
+            get_filename_component(MODULE_FILE_NAME "${MODULE_FILE}" NAME_WLE)
+            if("${MODULE_FILE_CONTENT}" STREQUAL "pragma Singleton\n")
+                set(QMLDIR_FILE_CONTENT "${QMLDIR_FILE_CONTENT}\nsingleton ${MODULE_FILE_NAME} 1.0 ${MODULE_FILE_NAME}.qml")
+            else()
+                set(QMLDIR_FILE_CONTENT "${QMLDIR_FILE_CONTENT}\n${MODULE_FILE_NAME} 1.0 ${MODULE_FILE_NAME}.qml")
+            endif()
+        endforeach()
+
+        set(QMLDIR_FILE_CONTENT "${QMLDIR_FILE_CONTENT}\n")
+
+        foreach(MODULES_INTERNAL_FILE ${MODULES_INTERNAL_FILES})
+            get_filename_component(MODULES_INTERNAL_FILE_NAME "${MODULES_INTERNAL_FILE}" NAME_WLE)
+            set(QMLDIR_FILE_CONTENT "${QMLDIR_FILE_CONTENT}\ninternal ${MODULES_INTERNAL_FILE_NAME} internal/${MODULES_INTERNAL_FILE_NAME}.qml")
+        endforeach()
+
+        file(WRITE "${ROOT_DIR}/qmldir" "${QMLDIR_FILE_CONTENT}")
+    endfunction()
+
+    create_modules_qmldir_files(${CMAKE_CURRENT_SOURCE_DIR}/assets/modules)
+
+    # Create assets qrc file
+    function(create_assets_qrc_file)
+        file(GLOB_RECURSE ASSETS_FILES ${CMAKE_CURRENT_SOURCE_DIR}/assets/*)
+        list(FILTER ASSETS_FILES EXCLUDE REGEX "${CMAKE_CURRENT_SOURCE_DIR}/assets/assets.qrc")
+
+        set(ASSETS_FILE_CONTENT "<RCC>\n    <qresource prefix=\"/\">")
+        foreach(ASSET_FILE ${ASSETS_FILES})
+            file(RELATIVE_PATH ASSET_FILE ${CMAKE_CURRENT_SOURCE_DIR}/assets "${ASSET_FILE}")
+            set(ASSETS_FILE_CONTENT "${ASSETS_FILE_CONTENT}\n        <file>${ASSET_FILE}</file>")
+        endforeach()
+        set(ASSETS_FILE_CONTENT "${ASSETS_FILE_CONTENT}\n    </qresource>\n</RCC>")
+        file(WRITE "${CMAKE_CURRENT_SOURCE_DIR}/assets/assets.qrc" "${ASSETS_FILE_CONTENT}")
+    endfunction()
+    create_assets_qrc_file()
+
+    # Include Qt resource files
+    qt5_add_resources(RESOURCES ${PROJECT_QT_RESOURCE})
+    target_sources(${TARGET} PRIVATE ${RESOURCES})
+
+endfunction()
